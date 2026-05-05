00363a848cf54f22c7a197688c7c548d29f07c2a
keyler3333 <kyturk42@gmail.com>
2026-05-05 05:33:49 -0400

    Create sw-registere.js

---
+++ sw-registere.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(reg => {
+            console.log('SW registered');
+        }).catch(err => {
+            console.warn('SW registration failed:', err);
+        });
+    });
+}
