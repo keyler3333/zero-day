86dfa119ef8415ca801b9d8fac30a3e63e23b494
keyler3333 <kyturk42@gmail.com>
2026-05-05 05:46:53 -0400

    Delete sw-registere.js

--- sw-registere.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(reg => {
-            console.log('SW registered');
-        }).catch(err => {
-            console.warn('SW registration failed:', err);
-        });
-    });
-}
